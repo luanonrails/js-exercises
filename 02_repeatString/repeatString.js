f235d3b1d8df04466611d75bef4f755036898b45
Luan Ferreira <181789192+luanonrails@users.noreply.github.com>
2025-01-02 13:48:11 -0300

    feat: Adiciona 02_repeatString

---
+++ 02_repeatString/repeatString.js
@@ -0,0 +1,10 @@
+const repeatString = function(word, times) {
+    if (times < 0) return "ERROR";
+    let newString = "";
+    for (let i = 0; i < times; i++) {
+        newString += word;
+    }
+    return newString;
+}
+
+module.exports = repeatString;
