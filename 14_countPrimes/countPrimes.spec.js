f39a2253d0c621dbee59d92a8c3dd8842932a683
Luan Ferreira <181789192+luanonrails@users.noreply.github.com>
2025-01-20 09:01:23 -0300

    feat: Adiciona mais dois testes

--- 14_countPrimes/countPrimes.spec.js
+++ 14_countPrimes/countPrimes.spec.js
@@ -4,11 +4,17 @@ describe('countPrimes', () => {
     test('prime numbers less than or equal 10', () => {
         expect(countPrimes(10)).toEqual(4);
     });
+    test('prime numbers less than or equal 11', () => {
+        expect(countPrimes(11)).toEqual(5);
+    });
     test('prime numbers less than or equal 100', () => {
         expect(countPrimes(100)).toEqual(25);
     });
     test('prime numbers less than or equal 1', () => {
         expect(countPrimes(1)).toEqual(0);
+    });
+    test('prime numbers less than or equal 9M', () => {
+        expect(countPrimes(9000000)).toEqual(602489);
     });
     test('doesn\'t accept negatives', () => {
         expect(countPrimes(-10)).toEqual('OOPS');
